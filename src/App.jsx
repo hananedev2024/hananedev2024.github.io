22c7b868a57391d95a2f5b1721e167024f42995b
hanane <aitoulhyanehanane@gmail.com>
2024-05-05 18:42:45 +0200

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import './App.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from 'Components/About';
-import Contact from 'Components/Contact';
-import Shop from 'Components/Shop';
-import NoPage from 'NoPage';
+import About from './components/About';
+import Contact from './Components/Contact';
+import Shop from './Components/Shop';
+import NoPage from './NoPage';
 
 function App() {
 
